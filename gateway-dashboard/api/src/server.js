0834161ec518e7dc3e79f1178f4a2e0582c9e743
Adnan-Safdari <safdariadnan@gmail.com>
2026-03-20 14:38:31 +0530

    Added express server start code for gateway dashboard and vulnerable app

---
+++ gateway-dashboard/api/src/server.js
@@ -0,0 +1,10 @@
+const dotenv = require("dotenv");
+const app = require("./app");
+
+dotenv.config();
+
+const PORT = Number(process.env.PORT) || 4000;
+
+app.listen(PORT, () => {
+	console.log(`API server running on port ${PORT}`);
+});
